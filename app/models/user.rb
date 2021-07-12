bf4156c4097e5955e662718a7369dc1b27ba88c9
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-07-12 09:39:37 +0800

    新增: 資料表 User 增加 name & avatar 欄位

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,13 @@ class User < ApplicationRecord
          :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
   
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      # user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
-    end
+    user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0, 20]
+    user.name = auth.info.name
+    user.avatar = auth.info.image
+
+    user.save
+    user
   end
 end
