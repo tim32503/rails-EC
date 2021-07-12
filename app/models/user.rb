0f181beb9775f692395f1e8dfe410b4ab88fdd37
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-07-12 11:51:39 +0800

    刪除: 移除 devise 更新資料須輸入密碼之功能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+         :rememberable, :omniauthable, omniauth_providers: [:facebook]
   
   def self.from_omniauth(auth)
     user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "users/registrations" }
   root "pages#index"
 end
